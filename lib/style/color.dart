10d1f8a579477ab66e8b125e6639fc116df88e9b
azzaaasss <mobilelegendsazza@gmail.com>
2023-12-18 02:55:41 +0700

    init

---
+++ lib/style/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color lightBrown = const Color(0xFFbaa292);
+Color brown = const Color(0xFFa48269);
+
+
+Color lightOrange = const Color(0xFFefc189);
+Color orange = const Color(0xFFe9a66d);
+
+Color textH1 = const Color(0xFF2d2d2b);
---
+++ lib/style/text.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tubes_iot/style/color.dart';
+
+TextStyle text_22_700 = GoogleFonts.poppins(
+    textStyle: TextStyle(
+        fontSize: 22, fontWeight: FontWeight.w700, height: 1.2, color: textH1));
+
+TextStyle text_12_300 = GoogleFonts.poppins(
+    textStyle: TextStyle(
+        fontSize: 12.0,
+        fontWeight: FontWeight.w300,
+        height: 1.5,
+        color: textH1.withOpacity(0.5)));
